89bcdbb674c9bd08b24d2c08bc33e0c8c5707e70
korbinian <korbinian.maier@tum.de>
2017-12-07 23:11:40 +0000

    Set Cursor in front of unit. Added Resource for constants. Get length of month from calendar.

--- app/src/main/java/com/example/kuabe/k_tracking/MainActivity.java
+++ app/src/main/java/com/example/kuabe/k_tracking/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.kuabe.k_tracking;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -19,7 +22,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button button = findViewById(R.id.button);
 
         button.setOnClickListener(this);
-        // display time in 'date# textview
+        // display time in 'date# textView
         int currDay = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
         int currMonth = Calendar.getInstance().get(Calendar.MONTH) + 1; // count starts at 0
         int currYear = Calendar.getInstance().get(Calendar.YEAR);
@@ -27,8 +30,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         TextView tvDate = findViewById(R.id.date);
         String currDateStr = currDay + "." + currMonth + "." + currYear;
         tvDate.setText(currDateStr);
+
+        // set cursor in etUsedData in front of unit
+        EditText etUsedData =  findViewById(R.id.usedData);
+        etUsedData.setSelection( 1 );
+        //TODO: open keyboard on opening
     }
 
+    @SuppressLint("SetTextI18n")
     @Override
     public void onClick(View view) {
         EditText etUsedData =  findViewById(R.id.usedData);
@@ -36,8 +45,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         TextView tvResultingDataPDay = findViewById(R.id.resDataPDay);
         TextView tvDailyAverage = findViewById(R.id.dailyAverage);
 
+
         // get current Day and Hour
         Calendar cal = Calendar.getInstance();
+        int currMonth = cal.get( Calendar.MONTH);
         int currDay = cal.get(Calendar.DAY_OF_MONTH);
         int currHour = cal.get(Calendar.HOUR);
 
@@ -47,15 +58,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         double expectedData;
 
         // do calculations for expected data
-        expectedData = usedData / (currDay + currHour / 24) * 30;
-        String exDatStr = String.format("%.2f", expectedData);
+        expectedData = usedData / (currDay + currHour / 24) * cal.getActualMaximum( currMonth );
+        @SuppressLint("DefaultLocale") String exDatStr = String.format("%.2f", expectedData);
         // calc resuming volume per day
-        double resData = 1000; // TODO add as resource
-        double resDataPDay = (resData - usedData) / ( 30 - currDay);
-        String resDataPDayStr = String.format("%.2f", resDataPDay);
+        int dataLimit = getResources().getInteger(R.integer.DataLimit);
+        double resDataPDay = (dataLimit - usedData) / ( cal.getActualMaximum( currMonth ) - currDay);
+        @SuppressLint("DefaultLocale") String resDataPDayStr = String.format("%.2f", resDataPDay);
         // calc daily average so far
         double dailyAverage = usedData / currDay;
-        String strDailyAverage = String.format("%.2f", dailyAverage);
+        @SuppressLint("DefaultLocale") String strDailyAverage = String.format("%.2f", dailyAverage);
 
         tvExpectedData.setText(  getResources().getText(R.string.expectedData) + exDatStr + getResources().getText(R.string.uniMB));
         tvResultingDataPDay.setText( getResources().getText(R.string.resData) + resDataPDayStr + getResources().getText(R.string.uniMB));
