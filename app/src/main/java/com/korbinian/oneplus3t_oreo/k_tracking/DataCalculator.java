f10cb0816b408d772630de60b042f3d1030b9105
Korbinian Maier <korbinian.maier@tum.de>
2017-12-21 16:47:55 +0000

    Bugfix: correct calculation of expected data

--- app/src/main/java/com/korbinian/oneplus3t_oreo/k_tracking/DataCalculator.java
+++ app/src/main/java/com/korbinian/oneplus3t_oreo/k_tracking/DataCalculator.java
@@ -12,17 +12,19 @@ import static java.util.Calendar.*;
 
 class DataCalculator {
 
+    // Constants for calling getCal()
     private static int CURR_HOUR = 1;
     private static int CURR_DAY = 2;
     private static int CURR_MAX_DAY = 3;
 
+    /* Function: returns current hour (0..23), day (1..31) and maximum of days in current month  */
     static private int getCal( int alt ) {
 
         Calendar cal = getInstance();
 
         switch( alt ){
             case 1:
-                return cal.get(HOUR);
+                return cal.get(HOUR_OF_DAY);
             case 2:
                 return cal.get(DAY_OF_MONTH);
             case 3:
@@ -33,47 +35,44 @@ class DataCalculator {
 
     }
 
-
-    static double calcAvgData( String inputUsedData ){
+    /* Function: calculates the average data used from beginning till now */
+    static double calcAvgData( double usedData ){
 
         int currDay = getCal( CURR_DAY );
-
-        double usedData = Double.parseDouble(inputUsedData.substring(0, inputUsedData.length() - 2));
 
         // calc daily average so far
         return usedData / currDay;
     }
 
-    static double calcExpectedData( String inputUsedData ){
+    /* Function: calculates the expected data based on the current usage */
+    static double calcExpectedData( double usedData ){
 
         int currHour = getCal( CURR_HOUR);
         int currDay = getCal( CURR_DAY);
         int maximum = getCal( CURR_MAX_DAY);
 
-        double usedData = Double.parseDouble(inputUsedData.substring(0, inputUsedData.length() - 2));
-
         // do calculations for expected data
-        return usedData / (currDay - 1 + currHour / 24) * maximum; //TODO: calculation slightly wrong
+        return (usedData * maximum) / (currDay - 1 + currHour / 24);
     }
 
-    static double calcRemainingDataPerDay( String inputUsedData, int dataLimit){
+    /* Function: calculates the average data for the remaining days to meet the data limit */
+    static double calcRemainingDataPerDay( double usedData, int dataLimit){
 
         int currDay = getCal( CURR_DAY);
         int maximum = getCal( CURR_MAX_DAY);
-
-        double usedData = Double.parseDouble(inputUsedData.substring(0, inputUsedData.length() - 2));
 
         // calc remaining volume per day
         return (dataLimit - usedData) / ( maximum - currDay);
     }
 
+    /* calculates the nominal value used till now with average usage per month */
     static double calcNominalUsedData( int dataLimit ){
 
         int currDay = getCal( CURR_DAY);
         int currHour = getCal( CURR_HOUR);
         int maximum = getCal( CURR_MAX_DAY);
 
-        return (dataLimit / maximum) * (currDay - 1 + currHour / 24);
+        return  (dataLimit / maximum) * ( currDay - 1 + (double) currHour / 23); //TODO: does not add up
     }
 
 }
--- app/src/main/java/com/korbinian/oneplus3t_oreo/k_tracking/MainActivity.java
+++ app/src/main/java/com/korbinian/oneplus3t_oreo/k_tracking/MainActivity.java
@@ -38,37 +38,42 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @SuppressLint("SetTextI18n")
     @Override
     public void onClick(View view) {
-        EditText etUsedData =  findViewById(R.id.usedData);
+        EditText etUsedData = findViewById(R.id.usedData);
         TextView tvExpectedData = findViewById(R.id.expectedData);
         TextView tvResultingDataPDay = findViewById(R.id.resDataPDay);
         TextView tvDailyAverage = findViewById(R.id.dailyAverage);
         TextView tvNominalUsedData = findViewById(R.id.nominalUsed);
 
         // Get data from edit text
-        String inputUsedData = etUsedData.getText().toString();
-        // calc average data
-        double dailyAverage = DataCalculator.calcAvgData( inputUsedData );
-        // calc expected Data to be used
-        double expectedData = DataCalculator.calcExpectedData( inputUsedData );
-        // calc remaining Data per day
-        int dataLimit = getResources().getInteger(R.integer.DataLimit);
-        double remDataPD = DataCalculator.calcRemainingDataPerDay( inputUsedData, dataLimit);
-        // calc nominal Used Data if average usage per day
-        double nominalUsedData = DataCalculator.calcNominalUsedData( dataLimit);
+        String inputUsedData = etUsedData.getText().toString(); //TODO: add check for zero etc.
+        double usedData = Double.parseDouble(inputUsedData.substring(0, inputUsedData.length() -2 ));
+        if (usedData == 0 || usedData < 0){
+            etUsedData.setText(getResources().getText(R.string.invalid));
+        } else {
+
+            // calc average data
+            double dailyAverage = DataCalculator.calcAvgData(usedData);
+            // calc expected Data to be used
+            double expectedData = DataCalculator.calcExpectedData(usedData);
+            // calc remaining Data per day
+            int dataLimit = getResources().getInteger(R.integer.DataLimit);
+            double remDataPD = DataCalculator.calcRemainingDataPerDay(usedData, dataLimit);
+            // calc nominal Used Data if average usage per day
+            double nominalUsedData = DataCalculator.calcNominalUsedData(dataLimit);
 
 
-        @SuppressLint("DefaultLocale") String exDatStr = String.format("%.2f", expectedData);
+            @SuppressLint("DefaultLocale") String exDatStr = String.format("%.2f", expectedData);
 
-        @SuppressLint("DefaultLocale") String remDataPDayStr = String.format("%.2f", remDataPD);
+            @SuppressLint("DefaultLocale") String remDataPDayStr = String.format("%.2f", remDataPD);
 
-        @SuppressLint("DefaultLocale") String strDailyAverage = String.format("%.2f", dailyAverage);
+            @SuppressLint("DefaultLocale") String strDailyAverage = String.format("%.2f", dailyAverage);
 
-        @SuppressLint("DefaultLocale") String strNominalUsed = String.format("%.2f", nominalUsedData);
+            @SuppressLint("DefaultLocale") String strNominalUsed = String.format("%.2f", nominalUsedData);
 
-        tvExpectedData.setText( getResources().getText(R.string.expectedData) + exDatStr + getResources().getText(R.string.uniMB) );
-        tvResultingDataPDay.setText( getResources().getText(R.string.resData) + remDataPDayStr + getResources().getText(R.string.uniMB));
-        tvDailyAverage.setText( getResources().getText(R.string.dailyAvg) + strDailyAverage + getResources().getText(R.string.uniMB));
-        tvNominalUsedData.setText( getResources().getText(R.string.nominalUsed) + strNominalUsed + getResources().getText(R.string.uniMB));
-
+            tvExpectedData.setText(getResources().getText(R.string.expectedData) + exDatStr + getResources().getText(R.string.uniMB));
+            tvResultingDataPDay.setText(getResources().getText(R.string.resData) + remDataPDayStr + getResources().getText(R.string.uniMB));
+            tvDailyAverage.setText(getResources().getText(R.string.dailyAvg) + strDailyAverage + getResources().getText(R.string.uniMB));
+            tvNominalUsedData.setText(getResources().getText(R.string.nominalUsed) + strNominalUsed + getResources().getText(R.string.uniMB));
+        }
     }
 }
